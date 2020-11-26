977bff3dd8d3996a476a84ef11f1eb73d59b410a
BJJoiner <joiner_brandon@columbusstate.edu>
2020-11-26 21:33:42 +0000

    update app.js

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,25 +1,51 @@
+import React, { Component }  from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TaskList from './TaskList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            todoList: ['item2', 'item 1'],
+            task: ''
+        }
+    }
+    render(){
+        return (
+            <div className="App">
+                <h1>To-do List</h1>
+                <form onSubmit={(e) =>this.addTodo(e)}>
+                    <input
+                        type='text'
+                        className='input'
+                        placeholder='Enter Todo Item'
+                        value={this.state.task}
+                        onChange={(e) => this.setState({task: e.target.value})}
+                    />
+                    <button type='submit'>Add Todo</button>
+                </form>
+                <TaskList title={'Pending Todo'} buttonText={'Done'} tasks={this.state.todoList} buttonFunction={this.removeTodo} />
+                <TaskList title={'Completed'} buttonText={"Delete"} tasks={['done']} buttonFunction={this.deleteTodo} />
+            </div>
+        );
+    }
+    addTodo(e){
+        e.preventDefault();
+        this.setState({task: '', todoList: [ ...this.state.todoList, this.state.task] });
+    }
+    removeTodo = key =>{
+        let todoList = this.state.todoList;
+        let index = todoList.indexOf(key);
+        if(index > -1){
+            todoList.splice(index, 1);
+            this.setState({todoList: todoList})
+        }
+    }
+
+    deleteTodo = key =>{
+        console.log(key)
+    }
 }
 
 export default App;
